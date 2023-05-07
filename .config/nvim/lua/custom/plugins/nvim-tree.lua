f1d441683aa519c94fb5c41943f9acf6883d7630
Eptarch <eptarch@gmail.com>
2023-05-07 19:57:57 +0500

    🔧 Revert arrows in file tree

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -8,17 +8,6 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvimtree.setup({
-	-- change folder arrow icons
-	renderer = {
-		icons = {
-			glyphs = {
-				folder = {
-					arrow_closed = "⮞", -- arrow when folder is closed
-					arrow_open = "⮟", -- arrow when folder is open
-				},
-			},
-		},
-	},
 	-- disable window_picker for
 	-- explorer to work well with
 	-- window splits
